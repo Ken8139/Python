2c59756319cf6bcbd920d7fb431e3c5a7b0a44db
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-05 14:47:22 +0530

    python user input

--- ken.py
+++ ken.py
@@ -1,13 +1,3 @@
-'''Author: kenaz mathukutty
-Date: 01/10/2024
-Version: 1.0
-Python program to get the student details
-'''
-name=input("Enter your name of the student")
-roll_number=int(input("Enter Roll Number"))
-roll_number=roll_number+1
-CGPA=float(input("enter your CGPA"))
-percentage_mark=CGPA*100
-print("Name of the sudent:",name)
-print("Roll number",roll_number)
-print("Mark percentage",percentage_mark,"%")
+enter_your_name=input("Enter your name:")
+enter_your_age=int(input("Enter your age:"))
+print("Hello,",enter_your_name,"! You are",enter_your_age,"your old")
