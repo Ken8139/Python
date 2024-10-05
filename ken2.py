49220a872dbdaf5871af057099f526488b4fd9fc
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-05 15:50:16 +0530

    arithmetic

---
+++ ken2.py
@@ -0,0 +1,9 @@
+a=10
+b=5
+sum=(a+b)
+division=(a/b)
+print("sum:",sum,",division:",division)
+print("is a greater than b?:",a>b)
+print("Are A and B are equal?:",a==b)
+print("Logical AND:",a>b & a!=b)
+print("Logical OR:",a>b or a!=b)
