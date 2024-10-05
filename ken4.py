8c06c3a22279dab8309b6eaec87f419fd875a0ab
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-05 16:22:48 +0530

    ken4.py

--- ken4.py
+++ ken4.py
@@ -1,1 +1,6 @@
+''Author: kenaz mathukutty
+Date: 05/10/2024
+Version: 1.0
+Python program to get the student details
+'''
 print("Twinkle, twinkle, little star,\n\t\t How I wonder what you are!\n\t\t\t\t Up above the world so high,\n\t\t\t\t Like a diamond in the sky.\n Twinkle, twinkle, little star,\n\t\t How I wonder what you are")
