759388b806834778e750d38e3410abb6a4ca2e27
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-05 16:21:13 +0530

    ken.py

--- ken.py
+++ ken.py
@@ -1,3 +1,8 @@
+''Author: kenaz mathukutty
+Date: 05/10/2024
+Version: 1.0
+Python program to get the student details
+'''
 enter_your_name=input("Enter your name:")
 enter_your_age=int(input("Enter your age:"))
 print("Hello,",enter_your_name,"! You are",enter_your_age,"your old")
