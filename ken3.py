fe6d0c30f76abf7f1e34cb2f2be2f98b78a4e69f
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-05 15:51:24 +0530

    math

---
+++ ken3.py
@@ -0,0 +1,9 @@
+import math
+number=int(input("Enter a number"))
+sqrt=math.sqrt(number)
+factorial=math.factorial(number)
+raised=math.pow(number,2)
+print("Square root of 5:",sqrt)
+print("Factorial of 5:",factorial)
+print("Raised to power 2:",raised)
+
