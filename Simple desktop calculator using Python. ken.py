614949e73a17a8090ae59848e27b4d83c15e1030
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-08 14:49:40 +0530

    Simple desktop calculator using Python.

---
+++ Simple desktop calculator using Python. ken.py
@@ -0,0 +1,15 @@
+numb1=int(input("enter first number:"))
+numb2=int(input("enter second number:"))
+numb3=int(input("enter thrid number:"))
+sum=numb1+numb2
+print("The sum of num1 and num2 is:",sum)
+sub=numb2-numb1
+print("The difference when num2 is subtracted from num1 is:",sub)
+multiply=numb1*numb2
+print( "The product of num1 and num2 is:",multiply)
+Divide=numb1/numb2
+print("The quotient when num1 is divided by num2 is:",Divide)
+Modulus=numb1%numb2
+print("The remainder when num1 is divided by num2 is:",Modulus)
+Arithmetic=(numb1+numb2)*numb3/2
+print("The result of (num1 + num2) * num3 / 2 is:",Arithmetic)
