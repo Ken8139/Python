d69e92d050d6bb5fed6ebb84afbc3eae414eb597
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-08 20:56:42 +0530

    ken string.py

--- ken string.py
+++ ken string.py
@@ -1,3 +1,9 @@
+'''Author: kenaz mathukutty
+Date: 088/10/2024
+Version: 1.0
+Python program to get the student details
+'''
+
 first_name=input("Enter first name")
 second_name=input("Enter second name")
 full_name=first_name+" "+second_name
