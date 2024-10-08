0dd0bc7c99cfceb1dee77e651fea9c45e7bed292
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-08 15:04:42 +0530

    string

---
+++ ken string.py
@@ -0,0 +1,7 @@
+first_name=input("Enter first name")
+second_name=input("Enter second name")
+full_name=first_name+" "+second_name
+print(full_name)
+lenght=len(first_name)
+extracted=full_name[:lenght]
+print(extracted)
