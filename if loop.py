ee911b32f3b98dc108aeb682f45613c5a521d396
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-15 15:54:50 +0530

    If Loop

---
+++ if loop.py
@@ -0,0 +1,13 @@
+'''
+Python Lab
+Author=Kenaz mathukutty
+Date=15/10/2024
+python program to check whether the given number
+'''
+limit=int(input("Enter the limit:"))
+odd_number=1
+count=0
+while count<limit:
+    print(odd_number,"\t",end="")
+    count+=1
+    odd_number+=2
