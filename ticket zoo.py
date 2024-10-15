2155cd3f9b0c6170a4781c08e695a0653bfa40e1
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-15 15:05:12 +0530

    Zoo ticket fee

---
+++ ticket zoo.py
@@ -0,0 +1,13 @@
+'''
+Python Lab
+Author=Kenaz mathukutty
+Date=15/10/2024
+python program
+'''
+Age=int(input("Age"))
+if Age<10:
+    print("zoo fee is 7")
+elif 10<=Age<60:
+    print("zoo fee is 10")
+else:
+    print("zoo fee is 5")
