e72eda1064047b74b45fb36b74616fcd315bb044
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-15 14:35:28 +0530

    determine the larger of two number

---
+++ large of two number.py
@@ -0,0 +1,12 @@
+'''
+Python Lab
+Author=Kenaz mathukutty
+Date=15/10/2024
+python program
+'''
+number=int(input("Enter first number"))
+number1=int(input("Enter second number"))
+if number>number1:
+    print("The given number:",number,"is larger")
+else:
+    print("The given number:",number1,"is larger")
