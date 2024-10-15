de18b1483cf914724a0edcfb5912982c2e68fe76
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-15 15:11:14 +0530

    Traffic light

---
+++ Traffic light.py
@@ -0,0 +1,15 @@
+'''
+Python Lab
+Author=Kenaz mathukutty
+Date=15/10/2024
+python program to check whether the given number
+'''
+
+color = (input("Enter the TRAFFIC LIGHT COLOR: "))
+
+if color == "red":
+    print("STOP")
+elif color == "green":
+    print("GO")
+else:
+    print("WAIT")
