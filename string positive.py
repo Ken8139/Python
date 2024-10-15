52feae5af985fd8f1910ab4b8dc26a868aa383cd
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-15 14:03:45 +0530

    positive or negative with if and else condition

---
+++ string positive.py
@@ -0,0 +1,11 @@
+'''
+Python Lab
+Author=Kenaz mathukutty
+Date=15/10/2024
+python program to check whether the given number
+'''
+number=int(input("Enter a number:"))
+if number>0:
+    print("The given number:",number,"is positive")
+else:
+    print("The given number:",number,"is Negative")
