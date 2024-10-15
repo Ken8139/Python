9bd8692a315a325d8dddac697a5ec0bc0aabc40f
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-15 16:32:39 +0530

    sum

---
+++ cc.py
@@ -0,0 +1,12 @@
+'''
+Python Lab
+Author=Kenaz mathukutty
+Date=15/10/2024
+python program
+'''
+numb=int(input("Enter a number:")
+sum=0
+while numb>0:
+    r=num%10
+    sum=sum+rnum=num//10
+print(sum)
