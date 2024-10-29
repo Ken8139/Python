810315dc07a36a804c24c963fde376fb21f67b50
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-29 14:54:09 +0530

    prime number

---
+++ check whether prime number .py
@@ -0,0 +1,15 @@
+'''
+Python Lab
+Author=Kenaz mathukutty
+Date=15/10/2024
+python program
+'''
+
+check_prime=int(input("Enter a number"))
+for i in range(2,(check_prime//2)+1):
+    if check_prime% i==0:
+        break
+if i==(check_prime//2):
+    print(f"The given number {check_prime} is prime")
+else:
+    print(f"The given number {check_prime} is not a prime")
