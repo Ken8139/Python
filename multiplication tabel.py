9be21840e4cc65a503816cc6a321a5c0dc5c1706
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-29 15:43:20 +0530

    Multiplication table

---
+++ multiplication tabel.py
@@ -0,0 +1,13 @@
+'''
+Python Lab
+Author=Kenaz mathukutty
+Date=29/10/2024
+python program
+'''
+
+number=int(input("Enter a number"))
+for i in range(1,13):
+    product=number*i
+    print(f"{number}*i={product}")
+
+
