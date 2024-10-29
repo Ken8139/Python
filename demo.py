7be3cec059988d493455d4b7aa892cf332cba3e6
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-29 16:01:15 +0530

    vowel

---
+++ demo.py
@@ -0,0 +1,14 @@
+'''
+Python Lab
+Author=Kenaz mathukutty
+Date=29/10/2024
+python program
+'''
+
+string_input=input("Enter a string")
+vowels="aeiouAEIOU"
+vowels_count=0
+for char in string_input:
+    if char in vowels:
+        vowels_count+=1
+print(f"Number of vowels in the given string {string_input}={vowels_count}")
