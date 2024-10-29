6079dbcc3e95204047a0fbcda80021f99aa82e12
Ken8139 <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-10-29 16:19:15 +0530

    vowels

--- demo.py
+++ demo.py
@@ -7,8 +7,12 @@ python program
 
 string_input=input("Enter a string")
 vowels="aeiouAEIOU"
+
 vowels_count=0
+constanece_count=0
 for char in string_input:
     if char in vowels:
         vowels_count+=1
-print(f"Number of vowels in the given string {string_input}={vowels_count}")
+    else:
+        constanece_count+=1
+print(f"Number of vowels in the given string {string_input}={vowels_count} \n Number of constanece {constanece_count}")
