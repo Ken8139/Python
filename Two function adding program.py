30d774db3ac906d640adb7b2a6ba4e73691980f3
Kenaz Mathukutty <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2024-12-03 10:35:04 +0530

    Two function add right angle

---
+++ Two function adding program.py
@@ -0,0 +1,10 @@
+'''Author=Kenaz mathukutty 
+   Date=30\11\2024
+   Program=Right Angle Triangle'''
+
+def add_two_numbers():
+    number1=20
+    number2=30
+    sum=number1+number2
+    print(sum)
+add_two_numbers()
